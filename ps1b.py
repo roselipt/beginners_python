3cd6c6776e15dfb60e13f085933a7d66cd09eea0
roselipt <outofastem@gmail.com>
2021-10-12 20:52:29 +0000

    1b working

--- ps1b.py
+++ ps1b.py
@@ -1,15 +1,24 @@
-#  Problem Set 1a, MIT Open Course 6.0001 from 2016
+#  Problem Set 1b, MIT Open Course 6.0001 from 2016
 
-#  Get user input for annual salary, a fraction to save monthly and total cost of dream house.
+#  (1b)  Adds a semiannual raise to be asked for as input as decimal and awarded every six months.
+
+#  (1a)  Get user input for annual salary, a fraction to save monthly and total cost of dream house.
 #  Assuming an annual return on investment of r = 0.04, use savings*r/12 to accumulate savings,
 #  and 0.25 * total cost to calculate down payment,
 #  print the number of months required to save the downpayment.
 #
 
-annual_salary = float(input("Enter your annual salary: "))
-portion_saved = float(input("Enter your monthly savings as decimal: "))
-total_cost = float(input("Enter the cost of your dreamhouse: "))
-print("Based on a savings of", portion_saved, "of a salary of", annual_salary, "and a house costing", total_cost)
+#annual_salary = float(input("Enter your annual salary: "))
+#portion_saved = float(input("Enter your monthly savings as decimal: "))
+#total_cost = float(input("Enter the cost of your dreamhouse: "))
+#semi_annual_raise = float(input("Enter your semi annual raise: "))
+#print("Based on a savings of", portion_saved, "of a salary of", annual_salary, "and a house costing", total_cost)
+
+annual_salary = float(120000)
+portion_saved = float(.05)
+total_cost = float(500000)
+semi_annual_raise = float(.03)
+
 
 month = 0
 current_savings = 0.0
@@ -26,6 +35,11 @@ while current_savings < down_payment :
     month += 1
     interest = current_savings*r/12
     current_savings += (monthly_savings + interest)
+    if month % 6 == 0 :
+        annual_salary *= (1 + semi_annual_raise)
+        monthly_savings *= (1 + semi_annual_raise)
+        after_raise = "{} raise to {:.2f}"
+        print(after_raise.format(month, annual_salary))
 
 print("Months:", month)
 #print("Saving", monthly_savings, " per month, you will be able to buy your house in", month, "months.")
