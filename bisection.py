f925a0ce6e8250c2204915577b46e9483339b499
roselipt <outofastem@gmail.com>
2021-10-14 15:40:27 +0000

    bisection started

---
+++ bisection.py
@@ -0,0 +1,31 @@
+#  Bisection search
+
+target = int(input("Please pick a number: "))
+
+print("Got it! Now the amazing Guess-mo Machine will guess your number!")
+
+attempt = 0
+lower_bound = 0
+upper_bound = 100
+found = False
+guess = 42
+
+while (not found) :
+    print(" (rubbing forehead) ... Guess-mo's first guess is", guess, "!")
+    attempt +=1
+    if guess == target :
+        print("Dat's it")
+        found = True
+    elif guess < target :
+        upper_bound = guess
+    else :
+        lower_bound = guess
+    print(guess, "isn't it and new range is", lower_bound, "to", upper_bound)
+    guess = int(lower_bound + (upper_bound - lower_bound)/2)
+    
+print("Guess-mo has done it again, guessing", guess, "in", attempt, "steps!")    
+        
+#        found = (input("You want me to change found?") == "y")
+    
+print("And that is show business.")
+
