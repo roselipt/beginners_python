7b3cdc91a67dcdcae85a84c91aa4f0ae2d1b546a
roselipt <outofastem@gmail.com>
2021-10-16 15:31:02 +0000

    working

--- bisection.py
+++ bisection.py
@@ -8,20 +8,20 @@ attempt = 0
 lower_bound = 0
 upper_bound = 100
 found = False
-guess = 42
+guess = 50
 
-while (not found) :
+while not found :
     print(" (rubbing forehead) ... Guess-mo's first guess is", guess, "!")
     attempt +=1
     if guess == target :
         print("Dat's it")
         found = True
-    elif guess < target :
-        upper_bound = guess
-    else :
+    elif (guess < target) :
         lower_bound = guess
+    else :
+        upper_bound = guess
     print(guess, "isn't it and new range is", lower_bound, "to", upper_bound)
-    guess = int(lower_bound + (upper_bound - lower_bound)/2)
+    guess = int(lower_bound + ((upper_bound - lower_bound)/2))
     
 print("Guess-mo has done it again, guessing", guess, "in", attempt, "steps!")    
         
