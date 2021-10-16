94ea4382382721c8475ef06b264607ff47ffc775
roselipt <outofastem@gmail.com>
2021-10-16 15:41:43 +0000

    cleaned up

--- bisection.py
+++ bisection.py
@@ -4,23 +4,25 @@ target = int(input("Please pick a number: "))
 
 print("Got it! Now the amazing Guess-mo Machine will guess your number!")
 
-attempt = 0
 lower_bound = 0
 upper_bound = 100
-found = False
 guess = 50
+attempt = 0
+count_words = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh"]
+found = False
 
 while not found :
-    print(" (rubbing forehead) ... Guess-mo's first guess is", guess, "!")
+    print(" (rubbing forehead) ... Guess-mo's", count_words[attempt], "guess is", guess, "!")
     attempt +=1
     if guess == target :
-        print("Dat's it")
+        print("\nDat's it")
         found = True
     elif (guess < target) :
         lower_bound = guess
+        print("Too low! Next guess will bisect", lower_bound, "and", upper_bound)
     else :
         upper_bound = guess
-    print(guess, "isn't it and new range is", lower_bound, "to", upper_bound)
+        print("Too high! Next guess will bisect", lower_bound, "and", upper_bound)
     guess = int(lower_bound + ((upper_bound - lower_bound)/2))
     
 print("Guess-mo has done it again, guessing", guess, "in", attempt, "steps!")    
