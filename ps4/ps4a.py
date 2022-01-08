93fa81550c8c15b300215c8d24c52dc3a748cf82
tombudny <outofastem@gmail.com>
2022-01-07 19:44:33 -0500

    ps4a working

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -22,8 +22,20 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
+    if len(sequence) == 1:
+        singleton = sequence
+        return [singleton]
+    else:
+        perms = []
+        for i in range(len(sequence)):
+            the_rest = get_permutations(sequence[:i] + sequence[i+1:])
+            for j in range(len(the_rest)):
+                perm = sequence[i] + the_rest[j]
+                perms.append(perm) 
 
-    pass #delete this line and replace with your code here
+            #perms.append(sequence[i] + get_permutations(sequence[:i] + sequence[i+1:]))
+        return perms
+#    pass #delete this line and replace with your code here
 
 if __name__ == '__main__':
 #    #EXAMPLE
@@ -35,6 +47,17 @@ if __name__ == '__main__':
 #    # Put three example test cases here (for your sanity, limit your inputs
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
-
-    pass #delete this line and replace with your code here
+    example_input = 'abcd'
+    print('Input:', example_input)   
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
+    example_input = 'bcd'
+    print('Input:', example_input)   
+    print('Expected Output:', ['bcd', 'bdc', 'cbd', 'cdb', 'dbc', 'dcb'])
+    print('Actual Output:', get_permutations(example_input))
+    example_input = 'ric'
+    print('Input:', example_input)   
+    print('Expected Output:', ['ric', 'rci', 'irc', 'icr', 'cri', 'cir'])
+    print('Actual Output:', get_permutations(example_input))
+    #pass #delete this line and replace with your code here
 
