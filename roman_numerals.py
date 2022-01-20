728c5abf0b51bea47599b0c5e30fb197c41e28d9
tombudny <outofastem@gmail.com>
2022-01-20 11:45:08 -0500

    following interview

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,31 +1,15 @@
 number = int(input("Enter a number "))
 
 print("Thank you,", number)
-remaining = number
-roman = ''
-if remaining >= 50:
-    fifties = remaining // 50
-    roman += fifties * 'L'
-    remaining = remaining % 50
-if remaining == 40:
-    roman += 'XL'
-    remaining -= 40
-if remaining >= 10:
-    tens = remaining // 10
-    roman += 'X'* tens
-    remaining %= 10
-if remaining == 9:
-    roman += 'IX'
-    remaining -= 9
-if remaining >= 5:
-    roman += 'V'
-    remaining -= 5
-if remaining == 4:
-    roman += 'IV'
-    remaining -= 4
-#  Take this if out, but think about it first
-if remaining >= 1:
-    roman += 'I'*remaining
-    remaining = 0    
 
-print(number, 'is', roman, 'plus', remaining, 'left.')
+rules = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400,'CD'),(100,'C'), (90, 'XC'), \
+        (50, 'L'),(40, 'XL'),(10, 'X'),(9, 'IX'),(5, 'V'),(4, 'IV'), (1, 'I')]
+
+remaining = number
+s = ''
+for val, symbol in rules:
+    while remaining >= val:
+        s += symbol
+        remaining -= val
+
+print('number is',s)
