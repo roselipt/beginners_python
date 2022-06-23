989e908f07403303764c78746843f722f4ded808
tombudny <outofastem@gmail.com>
2022-06-23 09:24:11 -0400

    not sure

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
 #    # Put three example test cases here (for your sanity, limit your inputs
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
-    example_input = 'abcd'
+    example_input = 'abc'
     print('Input:', example_input)   
     print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
     print('Actual Output:', get_permutations(example_input))
